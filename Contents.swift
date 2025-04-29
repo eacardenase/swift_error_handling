37a008c502fe78cb22f03075ac26b7f5ed9860c6
Edwin Cardenas <ecardenas@lwolf.com>
2025-04-29 16:07:46 -0500

    feat: add basic token and lexer

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,25 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+enum Token: CustomStringConvertible {
+    case number(Int)
+    case plus
+    
+    var description: String {
+        switch self {
+        case .number(let n):
+            return "Number: \(n)"
+        case .plus:
+            return "Symbol: +"
+        }
+    }
+}
+
+class Lexer {
+    let input: String
+    var position: String.Index
+    
+    init(input: String) {
+        self.input = input
+        self.position = input.startIndex
+    }
+}
