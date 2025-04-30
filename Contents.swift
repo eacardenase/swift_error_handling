6e4f2e44a3dec16b463ef8c7c6f3fb51e1d7f810
Edwin Cardenas <ecardenas@lwolf.com>
2025-04-30 18:52:57 -0500

    feat: get hole number token from input

--- Contents.swift
+++ Contents.swift
@@ -41,13 +41,34 @@ class Lexer {
         position = input.index(after: position)
     }
     
+    func getNumber() -> Int {
+        var value = 0
+        
+        while let nextCharacter = peek() {
+            switch nextCharacter {
+            case "0"..."9":
+                let digitValue = Int(String(nextCharacter))!
+                
+                value = 10 * value + digitValue
+                
+                advance()
+            default:
+                return value
+            }
+        }
+        
+        return value
+    }
+    
     func lex() throws -> [Token] {
         var tokens = [Token]()
         
         while let nextCharacter = peek() {
             switch nextCharacter {
             case "0"..."9":
-                break
+                let value = getNumber()
+                
+                tokens.append(.number(value))
             case "+":
                 tokens.append(.plus)
                 advance()
